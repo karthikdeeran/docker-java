6b8356b31a90638fc4b048c6092d6cac8bbbb428
karthikdeeran <kaarthik.surya@gmail.com>
2022-08-05 21:25:19 +0800

    Modified response

--- src/main/java/com/mkyong/StartApplication.java
+++ src/main/java/com/mkyong/StartApplication.java
@@ -2,19 +2,22 @@ package com.mkyong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class StartApplication {
 
     @GetMapping("/")
-    public String index(final Model model) {
-        model.addAttribute("title", "Docker + Spring Boot");
-        model.addAttribute("msg", "Welcome to the docker container!");
-        return "index";
+    @ResponseBody()
+    public ResponseEntity<String> index(final Model model) {
+      //  model.addAttribute("title", "Docker + Spring Boot");
+       // model.addAttribute("msg", "Welcome to the docker container!");
+        return ResponseEntity.ok("test");
     }
 
     public static void main(String[] args) {
